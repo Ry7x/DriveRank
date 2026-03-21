48f6c84c074915eadf20a6748812e95512308aab
Ry7x <96411438+Ry7x@users.noreply.github.com>
2026-03-21 02:15:05 +0100

    Remove Leaflet CSS import from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type {Metadata} from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import 'leaflet/dist/leaflet.css';
 import { Toaster } from "@/components/ui/toaster";
 import { FirebaseClientProvider } from "@/firebase/client-provider";
 import { DrivingProvider } from "@/context/driving-context";
